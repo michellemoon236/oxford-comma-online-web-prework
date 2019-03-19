211704610d4e38e138f73343d86d6e8757300ca3
Michelle M <michellemoon236@gmail.com>
2019-03-19 21:03:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,10 +10,18 @@ end
 def oxford_comma(array)
 
 if array.length == l
-  
+  array
+elsif array.length == 2 
 last_value = "and " + array.last
 array.pop
 array.join(", ")
 array << last_value
 array.join(", ")
+else
+last_value = "and " + array.last
+array.pop
+array.join(", ")
+array << last_value
+array.join(", ")
+end
 end
