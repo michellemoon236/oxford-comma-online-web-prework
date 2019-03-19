7f236181e5e53275b7360d8ee3af2ef3651dfdc7
Michelle M <michellemoon236@gmail.com>
2019-03-19 21:03:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,26 +2,6 @@ def oxford_comma(array)
 
 last_value = "and " + array.last
 array.pop
-array.join(", ")
 array << last_value
 array.join(", ")
-end
-
-def oxford_comma(array)
-
-if array.length == l
-  array
-elsif array.length == 2 
-last_value = "and " + array.last
-array.pop
-array.join(", ")
-array << last_value
-array.join(", ")
-else
-last_value = "and " + array.last
-array.pop
-array.join(", ")
-array << last_value
-array.join(", ")
-end
 end
