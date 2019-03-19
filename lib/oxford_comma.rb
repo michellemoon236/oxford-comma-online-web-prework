3f9d24bed7bfe0928989c9ac209cb16af66eebe8
Michelle M <michellemoon236@gmail.com>
2019-03-19 21:04:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,18 @@
 def oxford_comma(array)
 
+if array.length == l
+  array
+elsif array.length == 2 
 last_value = "and " + array.last
 array.pop
+array.join(", ")
 array << last_value
 array.join(", ")
+else
+last_value = "and " + array.last
+array.pop
+array.join(", ")
+array << last_value
+array.join(", ")
+end
 end
