d25ab9114b7fa5519a5fa2893910d2aa28a603c3
Michelle M <michellemoon236@gmail.com>
2019-03-19 21:10:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+if array.size == 1
+  array.first
+elsif array.size == 2 
+  last_value = "and " + array.last
+  array.pop
+  array << last_value
+  array.join(" ")
+else
+  last_value = "and " + array.last
+  array.pop
+  array << last_value
+  array.join(", ")
+end
 end
