f0762f6038204226970c82743d9d6342f693099c
Michelle M <michellemoon236@gmail.com>
2019-03-19 21:04:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,14 +5,12 @@ if array.length == l
 elsif array.length == 2 
   last_value = "and " + array.last
   array.pop
-  array.join(", ")
   array << last_value
-  array.join(", ")
+  array.join(" ")
 else
   last_value = "and " + array.last
   array.pop
-array.join(", ")
-array << last_value
-array.join(", ")
+  array << last_value
+  array.join(", ")
 end
 end
