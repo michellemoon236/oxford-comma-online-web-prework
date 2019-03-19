08725c46879e28f6ea8dedd840fa9f2230335d9a
Michelle M <michellemoon236@gmail.com>
2019-03-19 21:00:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,3 +6,13 @@ array.join(", ")
 array << last_value
 array.join(", ")
 end
+
+def oxford_comma(array)
+
+
+last_value = "and " + array.last
+array.pop
+array.join(", ")
+array << last_value
+array.join(", ")
+end
